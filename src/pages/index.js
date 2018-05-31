10bf3fc2349f1196b39a3b068d3a996742b90743
Shane Robbins <>
2018-05-31 11:29:53 -0400

    Back to working!

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 
 export default class IndexPage extends React.Component {
+<<<<<<< HEAD
 	render() {
 		const { data } = this.props;
 		const { edges: posts } = data.allMarkdownRemark;
@@ -17,6 +18,19 @@ export default class IndexPage extends React.Component {
 						<h2>{frontmatter.title}</h2>
 					</div>
 					{/* {posts
+=======
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+          </div>
+          {posts
+>>>>>>> parent of 4a21dc4... working home query
             .map(({ node: post }) => (
               <div
                 className="content"
@@ -39,11 +53,19 @@ export default class IndexPage extends React.Component {
                   </Link>
                 </p>
               </div>
+<<<<<<< HEAD
             ))} */}
 				</div>
 			</section>
 		);
 	}
+=======
+            ))}
+        </div>
+      </section>
+    )
+  }
+>>>>>>> parent of 4a21dc4... working home query
 }
 
 IndexPage.propTypes = {
@@ -55,6 +77,7 @@ IndexPage.propTypes = {
 };
 
 export const pageQuery = graphql`
+<<<<<<< HEAD
 	query IndexQuery {
 		allMarkdownRemark(filter: { frontmatter: { title: { eq: "Home" } } }) {
 			edges {
@@ -72,3 +95,27 @@ export const pageQuery = graphql`
 		}
 	}
 `;
+=======
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
+>>>>>>> parent of 4a21dc4... working home query
