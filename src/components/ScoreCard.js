7170abf94ce7d97cf092aee5e7c689ee906b2541
benche <ben.che9@gmail.com>
2018-06-06 17:46:05 -0400

    changed absolute to flexbox

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,61 +1,85 @@
 import React from "react";
 import styled from "styled-components";
 
-const ScoreCardName = styled.p`
-	font-size: 2vmax;
-	width: 45%;
-	position: absolute;
-	margin-top: -1.5vmax;
-	margin-left: 5vmax;
-	font-weight: 700;
-	border-bottom: 2px solid #333;
-	overflow: visible;
+const ScoreCardRankingWrapper = styled.div`
+	display:block;
+	box-shadow: 0 0 3px 3px rgba(1, 1, 1, 0.4);
+	background-color: white;
+  margin-bottom: 10%;
+  padding-left:5%;
 `;
+
+const ScoreCardUpperWrapper = styled.div`
+  display:flex;
+`
+
+const ScoreCardLowerWrapper = styled.div`
+  display:flex;
+`
 
 const ScoreCardRank = styled.h2`
 	font-size: 7vmax;
-	position: absolute;
-	margin-top: -7.5vmax;
+  justify-content: flex-start;
+  margin-top:-5vmax;
+  font-weight: 700;
+`;
+
+const ScoreCardName = styled.p`
+	font-size: 3.5vmax;
+  width: 50%;
+  justify-content: flex-start;
 	font-weight: 700;
+  border-bottom: 2px solid #333;
+  padding-bottom:0;
+  margin-left:4vmin;
 `;
 
 const ScoreCardSubheader = styled.p`
+  margin-top: 1vmax;
+  display: flex;
+  justify-content:flex-start;
 	font-size: 1.5vmax;
-	font-weight: 200;
-	margin-left: 5vmax;
-	margin-top: 1.75vmax;
+  font-weight: 200;
 `;
 
-const ScoreCardRankingWrapper = styled.div`
-	margin-bottom: 2vmax;
-	margin: auto;
-	width: 100%;
-	padding: 5%;
-	box-shadow: 0 0 3px 3px rgba(1, 1, 1, 0.4);
-	background-color: white;
-	margin-bottom: 10%;
-`;
+const ScoreCardMiddleWrapper = styled.div`
+  display:flex;
+  justify-content:space-between;
+`
 
 const ScoreCardContentContainer = styled.div`
+  width:100%;
 	padding-left: 5%;
-	padding-right: 5%;
+  padding-right: 5%;
 `;
 
+const ScoreCardListWrapper = styled.div`
+  align-items:flex-start;
+`
+
 const ScoreCardContentListItem = styled.li`
-	font-size: 1.5vmax;
+  font-size: 1.5vmax;
+  display:block;
 `;
 
 const ScoreCardAlignRight = styled.div`
-	text-align: right;
-	position: absolute;
-	right: 25%;
-	margin-top: -5vmax;
+  display:flex;
+  justify-content:flex-end;
+  margin-top:-3vmax;
 `;
 
 const ScoreCardContentScore = styled.span`
+  display:inline-block;
 	font-size: 5vmax;
 	font-weight: 700;
 `;
+
+const ScoreCardShowMore = styled.div`
+  height:100px;
+  width: 100px;
+  background-color:grey;
+  border-radius: 50%;
+`
 
 export default props => {
 	const {
@@ -73,23 +97,31 @@ export default props => {
 	} = props.data;
 	return (
 		<ScoreCardRankingWrapper>
-			<ScoreCardRank>{rank}</ScoreCardRank>
-			<ScoreCardName>{resteraunt_name}</ScoreCardName>
+      <ScoreCardUpperWrapper>
+        <ScoreCardRank>{rank}</ScoreCardRank>
+        <ScoreCardName>{resteraunt_name}</ScoreCardName>
+      </ScoreCardUpperWrapper>
+      
 			<ScoreCardSubheader>{sub}</ScoreCardSubheader>
-			<ScoreCardContentContainer>
-				<ol>
-					<ScoreCardContentListItem>
-						{highlights[0].highlight_data}
-					</ScoreCardContentListItem>
-					<ScoreCardContentListItem>Crispy</ScoreCardContentListItem>
-					<ScoreCardContentListItem>Crispy</ScoreCardContentListItem>
-				</ol>
-				<ScoreCardAlignRight>
-					<p>
-						<ScoreCardContentScore>{overall_score}</ScoreCardContentScore>/5
-					</p>
-				</ScoreCardAlignRight>
-			</ScoreCardContentContainer>
+
+      <ScoreCardMiddleWrapper>
+        <ScoreCardContentContainer>
+          <ScoreCardListWrapper>
+            <ScoreCardContentListItem>
+              {highlights[0].highlight_data}
+            </ScoreCardContentListItem>
+            <ScoreCardContentListItem>Crispy</ScoreCardContentListItem>
+            <ScoreCardContentListItem>Crispy</ScoreCardContentListItem>
+          </ScoreCardListWrapper>
+          <ScoreCardAlignRight>
+            <p>
+              <ScoreCardContentScore>{overall_score}</ScoreCardContentScore>/5
+            </p>
+          </ScoreCardAlignRight>
+        </ScoreCardContentContainer>
+      </ScoreCardMiddleWrapper>
+      <ScoreCardShowMore />
 		</ScoreCardRankingWrapper>
+
 	);
 };
