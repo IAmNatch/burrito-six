d6e99486753d6d6b900522958870803baf2fc7cc
Shane Robbins <shanerobbins@A-Wild-Macbook-Appeared.local>
2018-06-07 11:20:47 -0400

    fixed layout component background colors

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,12 +5,17 @@ import Helmet from "react-helmet";
 import Navbar from "../components/Navbar";
 import "./all.sass";
 
-const TemplateWrapper = ({ children }) => (
-	<div style={{ minHeight: '100vh', backgroundColor: '#00AF96'}}>
-		<Helmet title="Burri.to | Home" />
-		<div>{children()}</div>
-	</div>
-);
+const TemplateWrapper = ({ children }) => {
+		const {pathname} = window.location;
+		let background = pathname === "/" ? "#FDD112" : "#00AF96"
+
+		return (
+			<div style={{ minHeight: '100vh', backgroundColor: background}}>
+				<Helmet title="Burri.to | Home" />
+				<div>{children()}</div>
+			</div>
+	)
+};
 
 TemplateWrapper.propTypes = {
 	children: PropTypes.func,
