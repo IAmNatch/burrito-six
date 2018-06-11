0749d8d52e85ac530b8c275625008c000eec048f
Shane Robbins <shanerobbins@A-Wild-Macbook-Appeared.local>
2018-06-10 20:16:48 -0400

    hotfix for background

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,12 +5,14 @@ import Helmet from "react-helmet";
 import Navbar from "../components/Navbar";
 
 const TemplateWrapper = ({ children }) => {
-		const {pathname} = window.location;
-		let background = pathname === "/" ? "#FDD112" : "#00AF96"
+		// const {pathname} = window.location;
+		// let background = pathname === "/" ? "#FDD112" : "#00AF96"
+		let background = "#FDD112";
 
 		return (
 			<div style={{ minHeight: '100vh', backgroundColor: background}}>
 				<Helmet title="Burri.to | Home" >
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="stylesheet" type="text/css" href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css' />
 				</Helmet>
 				<div>{children()}</div>
