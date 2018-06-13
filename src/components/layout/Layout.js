ec972038bc57c85dbdc214675b861adf79d381f5
Shane Robbins <shanerobbins@A-Wild-Macbook-Appeared.local>
2018-06-12 20:04:37 -0400

    Fixed bottom background once and for all!

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const PageWrapperStyled = styled.div`
-    grid-template-rows: [nav]10vh [jumbo]30vh [content]60vw;
-    display: grid;
+	grid-template-rows: [nav]10vh [jumbo]30vh [content] auto;
+	display: grid;
 
-    width: 100vw;
-    min-height: 100vh;
-`
+	width: 100vw;
+	min-height: 100vh;
+`;
 
-export const PageWrapper = (props) => {
-    return (
-        <PageWrapperStyled>
-        {props.children}
-        </PageWrapperStyled>
-    );
-}
+export const PageWrapper = props => {
+	return <PageWrapperStyled>{props.children}</PageWrapperStyled>;
+};
