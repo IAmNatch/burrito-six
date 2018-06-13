0217323caeb6add73e6190015ad6f75d3a8fb326
Danielle Subject <daniellesubject@MBPe15C02RWM5MFVH3.local>
2018-06-12 20:56:49 -0400

    fixing more merge conflicts

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-//import styled from 'styled-components';
-import styled, { injectGlobal } from 'styled-components';
-
-injectGlobal`
-	body {
-		font-family: Roboto, sans-serif;
-	}
-`
-=======
 import React from "react";
 import styled from "styled-components";
->>>>>>> 734adbbcf3dfff5edfc2bc96a05f53ad4edfa87e
 
 const PageWrapperStyled = styled.div`
 	grid-template-rows: [nav]10vh [jumbo]30vh [content] auto;
